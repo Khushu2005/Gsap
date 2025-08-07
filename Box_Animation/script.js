93bebb088f7969e087b9674a9d8fa111569cc9a4
Khushi sharma <ks0541929@gmail.com>
2025-08-07 22:23:57 +0530

    first commit

---
+++ Box_Animation/script.js
@@ -0,0 +1,19 @@
+gsap.to("#box",{
+    rotate:"360%",
+    x:1200,
+    duration:4,
+    delay:1,
+    borderRadius:"50%",
+     backgroundColor:"crimson"
+
+})
+gsap.from("#box-2",{
+    rotate:"360%",
+    x:1200,
+    duration:4,
+    delay:1,
+    borderRadius:"50%",
+    backgroundColor:"green"
+
+})
+
